984d8a0de3a9e360feb9a264aa953c015e0cea80
tushhaare <164902897+tushhaare@users.noreply.github.com>
2026-05-23 16:53:00 +0530

    Delete intern directory

--- intern/script.js
+++
@@ -1,9 +0,0 @@
-// You can fetch intern data dynamically here using an ID or URL param
-console.log("Verification Page Loaded");
-// Optional: Disable right click or copy
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeydown = function (e) {
-  if (e.ctrlKey && (e.key === 'u' || e.key === 's' || e.key === 'c')) {
-    e.preventDefault();
-  }
-};
